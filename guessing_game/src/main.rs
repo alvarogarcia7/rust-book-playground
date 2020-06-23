e0712cc57e3802f9a07dde44300e9fcf21b7e46c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-06-23 02:10:49 +0200

    [GuessingGame] Read a guess an compare it to the secret number (Listing 2-5)

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
 
     println!("Please input your guess:");
 
@@ -11,4 +17,13 @@ fn main() {
       .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    let guessed_number : i32 = guess.trim().parse()
+      .expect("Cannot parse number");
+
+    match guessed_number.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => println!("You win!"),
+    }
 }
