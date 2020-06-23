b33ffca7fd5bf45eabd06fbce2a2260f82d175d9
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-06-23 02:25:41 +0200

    [GuessingGame] Refactor: extract explaining function to read the user guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,23 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+fn read_number() -> i32 {
+  loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        match guess.trim().parse() {
+          Ok(num) => return num,
+          Err(_) => continue,
+        };
+  }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -12,19 +29,7 @@ fn main() {
     println!("Please input your guess:");
 
     loop {
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        println!("You guessed: {}", guess);
-
-        let guess: i32 = match guess.trim().parse() {
-          Ok(num) => num,
-          Err(_) => continue,
-        };
-
+        let guess = read_number();
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
