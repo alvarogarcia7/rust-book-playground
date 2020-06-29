07d9cd0cb51fe19c8d7d1259856fe548d2656931
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-06-29 18:17:23 +0200

    [Adder] Add some benchmark testing (performance)

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,8 +1,29 @@
+#![feature(test)]
+
+extern crate test;
+
+pub fn add2(n: i32) -> i32 {
+    n + n
+}
+
 #[cfg(test)]
 mod tests {
+    use test::Bencher;
+    use super::*;
+
     #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[bench]
+    fn bench1(b: &mut Bencher) {
+        let n = test::black_box(1000);
+
+        let a = b.iter(|| {
+            (0..n).fold(0, |old, new| add2(old) + add2(new))
+        });
+        println!("{:?}", a);
     }
 
     #[test]
