60fbf11311f7db6bc191473b15ef1c0a57b6b0f7
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-06-29 18:17:35 +0200

    [Adder] Format (no functional change)

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -9,8 +9,8 @@ pub fn add2(n: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
-    use test::Bencher;
     use super::*;
+    use test::Bencher;
 
     #[test]
     fn exploration() {
@@ -22,15 +22,13 @@ mod tests {
         // black_box: https://doc.rust-lang.org/stable/unstable-book/library-features/test.html#gotcha-optimizations
         let n = test::black_box(10000);
 
-        let a = b.iter(|| {
-            (0..n).fold(0, |old, new| add2(old) + add2(new))
-        });
-        
+        let a = b.iter(|| (0..n).fold(0, |old, new| add2(old) + add2(new)));
+
         println!("{:?}", a);
     }
 
     #[test]
     fn another() {
-        panic!("Make this test fail!");      
+        panic!("Make this test fail!");
     }
 }
