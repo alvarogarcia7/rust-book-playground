d60d14fdbec38d8603af31110867223b16ad2cbf
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-08-03 17:47:30 +0200

    [Chapter8] Collections: Create and Iterate a HashMap

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,5 +1,9 @@
+use std::collections::HashMap;
 
 fn main() {
+
+    println!("Vector:");
+
     let mut v = vec![100, 32, 57];
 
     v.push(1);
@@ -7,4 +11,17 @@ fn main() {
     for i in &v {
         println!("{}", i);
     }
+
+    println!("Hash Map:");
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+
 }
