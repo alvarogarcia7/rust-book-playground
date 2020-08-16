87d053613369a31525be4b7fba0fb3ea88443d22
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-08-16 16:39:37 +0200

    Green: a test is empty by default

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,11 @@ mod tests {
     fn canary_test() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn a_vector_is_empty_by_default() {
+        let vector: Vec<i32> = Vec::new();
+
+        assert_eq!(vector.len(), 0);
+    }
 }
